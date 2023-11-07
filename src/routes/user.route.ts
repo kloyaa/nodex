6058cf155bbc79da34b420873eb88571567320ca
Kolya <madridano.kolya@gmail.com>
2023-11-07 16:17:37 +0800

    fix(user): refactor common middlewares

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -5,7 +5,12 @@ import { isAuthenticated } from '../_core/middlewares/jwt.middleware';
 
 const router = Router();
 
-router.post('/user/v1/profile', checkUserOrigin, isAuthenticated, createProfile as any);
-router.get('/user/v1/profile', checkUserOrigin, isAuthenticated, getProfileByAccessToken as any);
+const commonMiddlewares = [
+    checkUserOrigin,
+    isAuthenticated
+];
+
+router.post('/user/v1/profile', commonMiddlewares, createProfile as any);
+router.get('/user/v1/profile', commonMiddlewares, getProfileByAccessToken as any);
 
 export default router;
